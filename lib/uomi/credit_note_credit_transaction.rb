4d64ff27bf5c2490a0c992dcbef5d34de374533c
teaspoon <kwaad.lepel@gmail.com>
2016-10-17 11:48:33 +0200

    rename the transaction belongs_to relationship

--- lib/uomi/credit_note_credit_transaction.rb
+++ lib/uomi/credit_note_credit_transaction.rb
@@ -1,6 +1,6 @@
 module Uomi
   class CreditNoteCreditTransaction < ActiveRecord::Base
     belongs_to :credit_note
-    belongs_to :transaction
+    belongs_to :cn_transaction, class_name: 'Transaction'
   end
 end
